2809c313f426de75e4e1c456e4be154e7431b656
Alexey Popov <aleshaka@inbox.ru>
2023-05-28 18:59:30 +0300

    #2

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -8,82 +8,58 @@ from notes.forms import NoteForm
 from notes.models import Note
 
 
-class NoteListViewTestCase(TestCase):
+class NotesTestCase(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpassword'
-        )
-
-        self.note = Note.objects.create(
-            title='Test Note',
-            text='This is a test note',
-            slug='test-note',
-            author=self.user,
-        )
-
-    def test_note_list_view(self):
-        self.client.login(username='testuser', password='testpassword')
-
-        url = reverse('notes:list')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        self.assertIn(self.note, response.context['object_list'])
-
-
-class NotesListTest(TestCase):
-    def setUp(self):
+        # Создаем пользователей и заметки для тестов
         self.user1 = User.objects.create_user(
-            username='user1', password='password1'
+            username='user1',
+            password='password1'
         )
         self.user2 = User.objects.create_user(
-            username='user2', password='password2'
+            username='user2',
+            password='password2'
         )
-
         self.note1 = Note.objects.create(
-            title='Заметка 1', text='Текст заметки 1', author=self.user1
+            title='Заметка 1',
+            text='Текст заметки 1',
+            author=self.user1
         )
         self.note2 = Note.objects.create(
-            title='Заметка 2', text='Текст заметки 2', author=self.user2
-        )
-
-    def test_notes_list_only_contains_user_notes(self):
-        self.client.login(username='user1', password='password1')
-
-        url = reverse('notes:list')
-
-        response = self.client.get(url)
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-        notes = response.context['object_list']
-        self.assertEqual(len(notes), 1)
-        self.assertEqual(notes[0], self.note1)
-
-
-class NoteCreateUpdateFormTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpass'
+            title='Заметка 2',
+            text='Текст заметки 2',
+            author=self.user2
         )
         self.client = Client()
-        self.client.login(username='testuser', password='testpass')
+        self.client.force_login(self.user1)
+
+    def test_note_list_view(self):
+        # Тестирование просмотра списка заметок
+        url = reverse('notes:list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn(self.note1, response.context['object_list'])
 
     def test_note_create_form(self):
+        # Тестирование формы создания заметки
         response = self.client.get(reverse('notes:add'))
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIsInstance(response.context['form'], NoteForm)
 
     def test_note_update_form(self):
-        note = Note.objects.create(
-            title='Test Note', text='Test text', author=self.user
-        )
-
+        # Тестирование формы обновления заметки
         response = self.client.get(
-            reverse('notes:edit', kwargs={'slug': note.slug})
+            reverse('notes:edit', kwargs={'slug': self.note1.slug})
         )
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertIsInstance(response.context['form'], NoteForm)
-        self.assertEqual(response.context['form'].instance, note)
+        self.assertEqual(response.context['form'].instance, self.note1)
+
+    def test_notes_list_only_contains_user_notes(self):
+        # Тестирование списка заметок, содержащего только заметки пользователя
+        url = reverse('notes:list')
+        self.client.force_login(self.user2)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        notes = response.context['object_list']
+        self.assertEqual(len(notes), 1)
+        self.assertEqual(notes[0], self.note2)
--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -1,46 +1,17 @@
 from http import HTTPStatus
 
 from django.contrib.auth.models import User
-from django.shortcuts import resolve_url
-from django.test import Client, TestCase
-from django.urls import reverse
+from django.shortcuts import resolve_url, reverse
+from django.test import TestCase
 
 from notes.models import Note
 
 
-class HomeTestCase(TestCase):
-    def test_home_page_accessible(self):
-        response = self.client.get(reverse('notes:home'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-
-class NoteTestCase(TestCase):
+class NoteAppTestCase(TestCase):
     def setUp(self):
-        self.client = Client()
         self.user = User.objects.create_user(
             username='testuser', password='testpassword'
         )
-        self.client.login(username='testuser', password='testpassword')
-
-    def test_notes_list_access(self):
-        response = self.client.get('/notes/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'notes/list.html')
-
-    def test_note_create_access(self):
-        response = self.client.get('/add/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'notes/form.html')
-
-    def test_note_success_access(self):
-        response = self.client.get('/done/')
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'notes/success.html')
-
-
-class NotePageAccessTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
         self.author = User.objects.create_user(
             username='author', password='password'
         )
@@ -51,147 +22,104 @@ class NotePageAccessTestCase(TestCase):
             author=self.author, title='Заголовок', text='Текст заметки'
         )
 
-    def test_note_detail_access(self):
-        # Автор заметки должен иметь доступ к странице с подробной информацией о заметке
-        self.client.login(username='author', password='password')
-        url = reverse('notes:detail', kwargs={'slug': self.note.slug})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_home_page_accessible(self):
+        # Проверка доступности домашней страницы
+        test_cases = [
+            ('notes:home', 'notes/home.html'),
+        ]
+        for url_name, template_name in test_cases:
+            with self.subTest(url_name=url_name):
+                url = reverse(url_name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+                self.assertTemplateUsed(response, template_name)
 
-        # Другой пользователь не должен иметь доступ к странице с подробной информацией о заметке
-        self.client.login(username='other_user', password='password')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+    def test_pages_accessibility(self):
+        # Проверка доступности страниц Аутентифицированному пользователю
+        self.client.force_login(self.user)
+        test_cases = [
+            ('notes:list', 'notes/list.html'),
+            ('notes:success', 'notes/success.html'),
+            ('notes:add', 'notes/form.html'),
+        ]
+        for url_name, template_name in test_cases:
+            with self.subTest(url_name=url_name):
+                url = reverse(url_name)
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+                self.assertTemplateUsed(response, template_name)
 
-    def test_note_edit_access(self):
-        # Автор заметки должен иметь доступ к странице редактирования заметки
-        self.client.login(username='author', password='password')
-        url = reverse('notes:edit', kwargs={'slug': self.note.slug})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_authorized_user_access(self):
+        # проверка доступа cтраницы отдельной заметки,
+        # удаления и редактирования заметки
+        # для авторизированного пользователя
+        test_cases = [
+            ('detail', HTTPStatus.OK),
+            ('edit', HTTPStatus.OK),
+            ('delete', HTTPStatus.OK),
+        ]
 
-        # Другой пользователь не должен иметь доступ к странице редактирования заметки
-        self.client.login(username='other_user', password='password')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        for view_name, allowed_status in test_cases:
+            with self.subTest(view_name=view_name):
+                self.client.force_login(self.author)
+                url = reverse(
+                    f'notes:{view_name}', kwargs={'slug': self.note.slug}
+                )
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, allowed_status)
 
-    def test_note_delete_access(self):
-        # Автор заметки должен иметь доступ к странице удаления заметки
-        self.client.login(username='author', password='password')
-        url = reverse('notes:delete', kwargs={'slug': self.note.slug})
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.OK)
+    def test_unauthorized_user_access(self):
+        # проверка доступа cтраницы отдельной заметки,
+        # удаления и редактирования заметки
+        # для не авторизированного пользователя
+        test_cases = [
+            ('detail', HTTPStatus.NOT_FOUND),
+            ('edit', HTTPStatus.NOT_FOUND),
+            ('delete', HTTPStatus.NOT_FOUND),
+        ]
 
-        # Другой пользователь не должен иметь доступ к странице удаления заметки
-        self.client.login(username='other_user', password='password')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
+        for view_name, forbidden_status in test_cases:
+            with self.subTest(view_name=view_name):
+                self.client.force_login(self.other_user)
+                url = reverse(
+                    f'notes:{view_name}', kwargs={'slug': self.note.slug}
+                )
+                response = self.client.get(url)
+                self.assertEqual(response.status_code, forbidden_status)
 
+    def test_anonymous_user_redirect_to_login(self):
+        url_mappings = [
+            ('notes:list', []),
+            ('notes:success', []),
+            ('notes:add', []),
+            ('notes:detail', [self.note.slug]),
+            ('notes:edit', [self.note.slug]),
+            ('notes:delete', [self.note.slug]),
+        ]
+        for url_name, args in url_mappings:
+            with self.subTest(url_name=url_name):
+                if args:
+                    url = reverse(url_name, args=args)
+                else:
+                    url = reverse(url_name)
+                response = self.client.get(url)
+                expected_redirect_url = (
+                    resolve_url('/auth/login/') + '?next=' + url
+                )
+                self.assertEqual(response.status_code, HTTPStatus.FOUND)
+                self.assertRedirects(response, expected_redirect_url)
 
-class NoteListPageAccessTestCase(TestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser', password='testpassword'
-        )
+    def test_pages(self):
+        # проверка Страницы регистрации пользователей,
+        # входа в учётную запись и выхода из неё доступны всем пользователям
+        page_templates = [
+            ('users:login', 'registration/login.html'),
+            ('users:logout', 'registration/logout.html'),
+            ('users:signup', 'registration/signup.html'),
+        ]
 
-        self.note = Note.objects.create(
-            title='Test Note', text='This is a test note', author=self.user
-        )
-
-    def test_anonymous_user_redirect_to_login_on_notes_list(self):
-        # Анонимный пользователь пытается получить доступ к странице списка заметок
-        response = self.client.get(reverse('notes:list'))
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/') + '?next=' + reverse('notes:list'),
-        )
-
-    def test_anonymous_user_redirect_to_login_on_note_success(self):
-        # Анонимный пользователь пытается получить доступ к странице успешного добавления заметки
-        response = self.client.get(reverse('notes:success'))
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/') + '?next=' + reverse('notes:success'),
-        )
-
-    def test_anonymous_user_redirect_to_login_on_note_create(self):
-        # Анонимный пользователь пытается получить доступ к странице создания заметки
-        response = self.client.get(reverse('notes:add'))
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/') + '?next=' + reverse('notes:add'),
-        )
-
-    def test_anonymous_user_redirect_to_login_on_note_detail(self):
-        # Анонимный пользователь пытается получить доступ к странице отдельной заметки
-        response = self.client.get(
-            reverse('notes:detail', args=[self.note.slug])
-        )
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/')
-            + '?next='
-            + reverse('notes:detail', args=[self.note.slug]),
-        )
-
-    def test_anonymous_user_redirect_to_login_on_note_update(self):
-        # Анонимный пользователь пытается получить доступ к странице редактирования заметки
-        response = self.client.get(
-            reverse('notes:edit', args=[self.note.slug])
-        )
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/')
-            + '?next='
-            + reverse('notes:edit', args=[self.note.slug]),
-        )
-
-    def test_anonymous_user_redirect_to_login_on_note_delete(self):
-        # Анонимный пользователь пытается получить доступ к странице удаления заметки
-        response = self.client.get(
-            reverse('notes:delete', args=[self.note.slug])
-        )
-
-        self.assertEqual(response.status_code, HTTPStatus.FOUND)
-        self.assertRedirects(
-            response,
-            resolve_url('/auth/login/')
-            + '?next='
-            + reverse('notes:delete', args=[self.note.slug]),
-        )
-
-
-class AuthenticationTestCase(TestCase):
-    def setUp(self):
-        self.username = 'testuser'
-        self.password = 'testpassword'
-        self.user = User.objects.create_user(
-            username=self.username, password=self.password
-        )
-
-    def test_login_page(self):
-        response = self.client.get(reverse('users:login'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'registration/login.html')
-
-    def test_logout_user(self):
-        # Проверка выхода из учетной записи
-        self.client.login(username=self.username, password=self.password)
-        response = self.client.get(reverse('users:logout'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_signup_page(self):
-        # Проверка доступности страницы регистрации
-        response = self.client.get(reverse('users:signup'))
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-        self.assertTemplateUsed(response, 'registration/signup.html')
+        for url_name, template_name in page_templates:
+            with self.subTest(url=url_name, template=template_name):
+                response = self.client.get(reverse(url_name))
+                self.assertEqual(response.status_code, HTTPStatus.OK)
+                self.assertTemplateUsed(response, template_name)
